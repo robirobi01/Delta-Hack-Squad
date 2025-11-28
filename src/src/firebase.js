33574424c5ffaab17e7a71817f3d3fb9d24744d8
Ritish <ritishsingha899@gmail.com>
2025-11-28 21:39:50 +0600

    Add Firebase configuration to firebase.js
    For Auth purpose.

---
+++ src/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC1-zgdj3JG6dKzHfaonfnEwAPxzNHsULY",
+  authDomain: "harvestguard.firebaseapp.com",
+  projectId: "harvestguard",
+  storageBucket: "harvestguard.firebasestorage.app",
+  messagingSenderId: "1001684743908",
+  appId: "1:1001684743908:web:758d1d75a8d5b56a17def1",
+  measurementId: "G-TQ903V7BFE"
+};
